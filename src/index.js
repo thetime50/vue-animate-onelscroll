2c1d96210f2b7bbde107ed7a6d4d5ad47c3785ea
luoxin <thetime_lx@qq.com>
2021-09-07 11:31:30 +0800

    * checkScrollEl  * 如果滚动元素是html 那么 动画元素可以是任何元素  * 如果滚动元素不是html 那么 动画元素的offsetTop  *  * scrollEl 为null 时不初始化  * 添加edge修饰符

--- src/index.js
+++ src/index.js
@@ -1,57 +1,131 @@
 import ScrollAnimate from './scroll-animate'
 
-function getScrollEl(el,binding){
-        let {name,value,oldValue,expression,arg,modifiers} = binding
-        let elParam = value.el
-        let scrollEl = window
-        if (typeof elParam == 'string'){
-          scrollEl = document.querySelector(elParam)
-        } else if (elParam instanceof Element || elParam instanceof HTMLDocument) {
-          scrollEl = elParam
-        }
-        return scrollEl
+function getScrollEl(el, binding) {
+    let { name, value, oldValue, expression, arg, modifiers } = binding
+    let elParam = value.scrollEl
+    let scrollEl = window
+    if (typeof elParam == 'string') {
+        scrollEl = document.querySelector(elParam)
+    } else if (elParam instanceof Element || elParam instanceof HTMLDocument) {
+        scrollEl = elParam
+    } else if (elParam=== null) {
+        scrollEl = null
+    }
+    return scrollEl
 }
 
 function removeEvent(el) {
-  let scrollEl = el._onelscroll_scrollEl
-  let scrollCb = el._onelscroll_scrollCb
-  if (scrollEl && scrollCb) {
-    scrollEl.removeEventListener('scroll', scrollCb)
-  }
-  el._onelscroll_scrollEl = null
-  el._onelscroll_scrollCb = null
-  return scrollCb // 可能后面有用
+    if (!el._onelscroll){
+        return
+    }
+    let scrollEl = el._onelscroll.scrollEl
+    let scrollCb = el._onelscroll.scrollCb
+    if (scrollEl && scrollCb) {
+        scrollEl.removeEventListener('scroll', scrollCb)
+    }
+    el._onelscroll = null
 }
 
-export default {
-  ScrollAnimate,
-  install(Vue) {
-    Vue.directive('onelscroll', {
-      inserted(el, binding) {
-        let scrollEl = getScrollEl(el, binding)
-        const scrollAnimate = ScrollAnimate(Date.now())
+/**
+ * checkScrollEl
+ * 如果滚动元素是html 那么 动画元素可以是任何元素
+ * 如果滚动元素不是html 那么 动画元素的offsetTop
+ * 
+ * scrollEl 为null 时不初始化
+ * 添加edge修饰符
+ */
+function checkScrollEl(scrollEl,el){
+    if (!scrollEl){
+        return false
+    }
+    if(scrollEl != window && el.parentNode.scrollHeight != scrollEl.scrollHeight){ //可能不是直接的子元素 但是至少要满足高度一致
+        if(el.parentNode!=scrollEl ){
+            if( el.parentNode.clientHeight!=el.parentNode.scrollHeight ){
+                console.warn('warning 内部有滚动元素','el.parentNode',el.parentNode,
+                    'el.parentNode.clientHeight',el.parentNode.clientHeight,
+                    'el.parentNode.scrollHeight',el.parentNode.scrollHeight
+                )
+                return false
+            } 
+            if(el.parentNode.clientHeight != scrollEl.scrollHeight){
+                console.warn('warning 滚动高度不匹配',
+                    'el.parentNode',el.parentNode,'scrollEl',scrollEl,
+                    'el.parentNode.clientHeight',el.parentNode.clientHeight,
+                    'scrollEl.scrollHeight',scrollEl.scrollHeight,
+                )
+                return false
+            }
+        }
+    }
+    return true
+}
+
+
+function getScrollCb(scrollEl, el, binding) {
+    const scrollAnimate = ScrollAnimate(Date.now(), scrollEl, el)
+    if(scrollEl == window){ // 原本针对window的动画效果
         const previousClassName = el.className
         let lastScrollTop = scrollEl.pageYOffset
-        const scrollCb = function() {
-          let scrollTop = scrollEl.pageYOffset || document.documentElement.scrollTop
-          const isUpwards = scrollTop < lastScrollTop
-          scrollAnimate.run(el, binding, {isUpwards, previousClassName},scrollEl)
-          lastScrollTop = scrollTop <= 0 ? 0 : scrollTop
+        scrollAnimate.scrollEl = scrollEl
+        scrollAnimate.scrollCb = function () {
+            let scrollTop = scrollEl.pageYOffset || document.documentElement.scrollTop
+            const isUpwards = scrollTop < lastScrollTop
+            scrollAnimate.run(el, binding, {
+                isUpwards, // 方向
+                previousClassName,
+            })
+            lastScrollTop = scrollTop <= 0 ? 0 : scrollTop
         }
-        el._onelscroll_scrollCb = scrollCb
-        el._onelscroll_scrollEl = scrollEl
-        scrollEl.addEventListener('scroll', scrollCb, false)
-      },
-      unbind(el, binding, vnode, oldVnode) {
-        removeEvent(el)
-      },
-      update(el, binding, vnode, oldVnode) {
-          let scrollEl = getScrollEl(el, binding)
-          let scrollCb = removeEvent(el)
-          el._onelscroll_scrollCb = scrollCb
-          el._onelscroll_scrollEl = scrollEl
-          scrollEl.addEventListener('scroll', scrollCb, false)
-      },
-    })
-  }
+    }else{
+        const previousClassName = el.className
+        let lastScrollTop = scrollEl.scrollTop
+        scrollAnimate.scrollEl = scrollEl
+        scrollAnimate.scrollCb = function () {
+            let scrollTop = scrollEl.scrollTop
+            const isUpwards = scrollTop < lastScrollTop
+            // console.log('scrollCb',scrollEl,scrollTop , lastScrollTop, isUpwards)
+            scrollAnimate.run(el, binding, {
+                isUpwards, // 方向
+                previousClassName,
+                scrollEl,
+            })
+            lastScrollTop = scrollTop <= 0 ? 0 : scrollTop
+        }
+    }
+    return scrollAnimate
+}
+
+/**
+ * 仅根据方向插入动画效果
+ */
+
+export default {
+    ScrollAnimate,
+    install(Vue) {
+        Vue.directive('onelscroll', {
+            inserted(el, binding) {
+                let scrollEl = getScrollEl(el, binding)
+                if(!checkScrollEl(scrollEl,el)){
+                    return
+                }
+                let scrollAnimate = getScrollCb(scrollEl, el, binding)
+                el._onelscroll = scrollAnimate
+                scrollEl.addEventListener('scroll', scrollAnimate.scrollCb, false)
+            },
+            unbind(el, binding, vnode, oldVnode) {
+                removeEvent(el)
+            },
+            update(el, binding, vnode, oldVnode) {
+                removeEvent(el)
+                let scrollEl = getScrollEl(el, binding)
+                let scrollCb = getScrollCb(scrollEl, el, binding)
+                if(!checkScrollEl(scrollEl,el)){
+                    return
+                }
+                let scrollAnimate = getScrollCb(scrollEl, el, binding)
+                el._onelscroll = scrollAnimate
+                scrollEl.addEventListener('scroll', scrollAnimate.scrollCb, false)
+            },
+        })
+    }
 }
--- src/scroll-animate.js
+++ src/scroll-animate.js
@@ -1,8 +1,10 @@
-export default () => {
+export default (date, scrollEl, el) => {
 
   const getClientHeight = () => document.documentElement.clientHeight
 
   const isInScrollView = ({top, bottom}) => top < getClientHeight() && bottom > 0
+
+  const isInElScrollView = ({top, bottom, scrollEl}) => top < scrollEl.clientHeight && bottom > 0
   
   const isDirectionAgnostic = (params) => typeof params === 'string'
 
@@ -15,18 +17,45 @@ export default () => {
 
   const applyAnimationClass = (el, current, newClass = '') => el.className = `${current} ${newClass}`.trim()
 
-  return {
+  let res = {
+    lastIsInView:false,
     isInView: isInScrollView,
-    run(el, {value: params, modifiers}, {isUpwards, previousClassName = ''}) {
+    isInElView:isInElScrollView,
+    getIsInView(scrollEl,el) {
+        let isInView = false
+        if (scrollEl == window) {
+            isInView = this.isInView(el.getBoundingClientRect())
+        } else {
+            isInView = this.isInElView({
+                top: el.offsetTop - scrollEl.scrollTop,
+                bottom: el.offsetTop + el.clientHeight - scrollEl.scrollTop,
+                scrollEl,
+            })
+        }
+        return isInView
+    },
+    run(el, {value: params, modifiers}, 
+        {isUpwards, previousClassName = '',scrollEl=window}) {
+      
+      let isInView = this.getIsInView(scrollEl, el)
+      if (modifiers.edge ){
+          if(this.lastIsInView == isInView) {
+            if (!isInView){
+                return applyAnimationClass(el, previousClassName)
+            }
+            return
+          }
+      }
+      this.lastIsInView = isInView
 
-      if(!this.isInView(el.getBoundingClientRect())) {
+      if(!isInView) {
         if (modifiers.repeat && isDirectionAgnostic(params)) {
           return applyAnimationClass(el, previousClassName)
         }
         return
       }
 
-      if (isDirectionAgnostic(params)) {
+      if (isDirectionAgnostic(params)) { // 字符串参数
         return applyAnimationClass(el, previousClassName, params)
       }
 
@@ -37,11 +66,16 @@ export default () => {
         return applyAnimationClass(el, previousClassName, animationClass)
       }
 
-      if(shouldResetAnimation({params, isUpwards, ...modifiers})) {
+      if(shouldResetAnimation({params, isUpwards, ...modifiers})) { // 如果当前滚动方向有配置参数类目
         return applyAnimationClass(el, previousClassName)
       }
 
     }
   }
+  
+  if(scrollEl && el){ // 单元测试还没处理
+      res.lastIsInView = res.getIsInView(scrollEl, el)
+  } 
+  return res
   
 }
